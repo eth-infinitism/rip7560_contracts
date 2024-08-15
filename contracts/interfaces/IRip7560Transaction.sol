066904d6a2bf4acb0566c9a47e399a6b28a8b815
Alex Forshtat <forshtat1@gmail.com>
2024-08-15 14:47:40 +0200

    Check in RIP-7560 related contracts

---
+++ contracts/interfaces/IRip7560Transaction.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+struct RIP7560TransactionStruct {
+    address sender;
+    uint256 nonce;
+    uint256 validationGasLimit;
+    uint256 paymasterValidationGasLimit;
+    uint256 postOpGasLimit;
+    uint256 callGasLimit;
+    uint256 maxFeePerGas;
+    uint256 maxPriorityFeePerGas;
+    uint256 builderFee;
+    address paymaster;
+    bytes paymasterData;
+    address deployer;
+    bytes deployerData;
+    bytes callData;
+    bytes signature;
+}
---
+++ contracts/utils/RIP7560Utils.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "../interfaces/IRip7560EntryPoint.sol";
+import "../interfaces/IRip7560Transaction.sol";
+
+address constant ENTRY_POINT = 0x0000000000000000000000000000000000007560;
+
+library RIP7560Utils {
+
+    //struct version, as defined in RIP-7560
+    uint constant VERSION = 0;
+
+    function decodeTransaction(uint256 version, bytes calldata transaction) internal pure returns (RIP7560TransactionStruct memory) {
+        require(version == VERSION, "RIP7560Utils: unsupported version");
+        return abi.decode(transaction, (RIP7560TransactionStruct));
+    }
+
+    function accountAcceptTransaction(
+        uint48 validAfter,
+        uint48 validUntil
+    ) internal {
+        (bool success,) = ENTRY_POINT.call(
+            abi.encodeCall(IRip7560EntryPoint.acceptAccount, (validAfter, validUntil))
+        );
+        require(success);
+    }
+
+    function paymasterAcceptTransaction(
+        bytes memory context,
+        uint256 validAfter,
+        uint256 validUntil
+    ) internal {
+        (bool success,) = ENTRY_POINT.call(
+            abi.encodeCall(IRip7560EntryPoint.acceptPaymaster, (validAfter, validUntil, context))
+        );
+        require(success);
+    }
+}
