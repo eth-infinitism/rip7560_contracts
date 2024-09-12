838e8457955e8d277d2f68e073ea8d70918f579e
Alex Forshtat <forshtat1@gmail.com>
2024-09-12 13:55:49 +0200

    Re-introduce 'RIP7560AccountDeployed' event

--- contracts/interfaces/IRip7560EntryPoint.sol
+++ contracts/interfaces/IRip7560EntryPoint.sol
@@ -8,25 +8,32 @@ interface IRip7560EntryPoint {
     function sigFailPaymaster(uint256 validAfter, uint256 validUntil, bytes calldata context) external;
 
 
-    /***
+    /**
      * An event emitted after each successful request.
-     * @param sender        - The account that generates this request.
-     * @param paymaster     - If non-null, the paymaster that pays for this request.
-     * @param deployer      - If non-null, the deployer that creates the sender contract.
-     * @param nonceKey      - The nonce key value from the request.
-     * @param nonceSequence - The nonce sequence value from the request.
-     * @param success       - True if the sender transaction succeeded, false if reverted.
-     * @param actualGasCost - Actual amount paid (by account or paymaster) for this UserOperation.
-     * @param actualGasUsed - Total gas used by this UserOperation (including preVerification, creation,
-     *                        validation and execution).
+     * @param sender          - The account that generates this request.
+     * @param paymaster       - If non-null, the paymaster that pays for this request.
+     * @param nonceKey        - The nonce key value from the request.
+     * @param nonceSequence   - The nonce sequence value from the request.
+     * @param executionStatus - True if the sender transaction succeeded, false if reverted.
      */
     event RIP7560TransactionEvent(
         address indexed sender,
         address indexed paymaster,
-        address indexed deployer,
         uint256 nonceKey,
         uint256 nonceSequence,
         uint256 executionStatus
+    );
+
+    /**
+     * An event emitted after "sender" account has been deployed.
+     * @param sender     - The account that has been deployed.
+     * @param paymaster  - The paymaster used by this transaction.
+     * @param deployer   - The deployer factory contract used to deploy this account.
+     */
+    event RIP7560AccountDeployed(
+        address indexed sender,
+        address indexed paymaster,
+        address indexed deployer
     );
 
     /**
